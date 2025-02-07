75c5e37a4a0c38794a022f955b7f580240837256
Ali <cafarovali2004@gmail.com>
2025-02-07 23:12:59 +0400

    first commit

---
+++ LoadingDictionary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public static class LoadingDictionary
+{
+    public static Dictionary<string, string> LoadDictionary(string filePath)
+    {
+        Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+        try
+        {
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split('-');
+                if (parts.Length == 2)
+                {
+                    dictionary[parts[0].ToLower()] = parts[1].ToLower();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error loading the dictionary file: " + ex.Message);
+        }
+
+        return dictionary;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Translator
+{
+    class Program
+    {
+        static void Main()
+        {
+            string filePath = @"D:\C#\Translator\dictionary.txt";
+            Dictionary<string, string> dictionary = LoadingDictionary.LoadDictionary(filePath);
+
+            if (dictionary.Count == 0)
+            {
+                Console.WriteLine("The dictionary is empty or could not be loaded.");
+                return;
+            }
+            Console.WriteLine("If you enter 0, program will close!");
+            Console.WriteLine();
+            while (true)
+            {
+                Random random = new Random();
+                List<string> englishWords = new List<string>(dictionary.Keys);
+                string selectedEnglishWord = englishWords[random.Next(englishWords.Count)];
+
+                Console.Write($"'{selectedEnglishWord}'-");
+                string userTranslation = Console.ReadLine().ToLower();
+
+                if (userTranslation == dictionary[selectedEnglishWord])
+                {
+                    Console.WriteLine("Success! Your translation is correct.");
+                }
+                else if (userTranslation == "0")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"The correct translation is: {dictionary[selectedEnglishWord]}");
+                }
+            }
+        }
+    }
+}
